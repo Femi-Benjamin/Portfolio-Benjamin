3f17e8ed439fec7a1667e30003e592fcb143fc3f
Olufemi Benjamin <benjaminolufemi16@gmail.com>
2024-02-11 01:03:04 -0600

    portfolio work

---
+++ src/app/components/Footer.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer className="footer border z-10 border-t-[#33353F] border-l-transparent border-r-transparent text-white">
+      <div className="containe p-12 flex justify-between xl:text-2xl text-xl">
+        <span>LOGO</span>
+        <p className="text-slate-600">All Right Reserved</p>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
